debc7f39260f280115a2ed36c7446e0aee8da004
Aman Khandelwal <aman.khandelwal.howrah@gmail.com>
2018-08-16 16:18:39 +0530

    Transactions Main Logic And basic UI done

---
+++ src/Transaction/index.js
@@ -0,0 +1,113 @@
+import React from 'react';
+import Card from '../Card';
+import Header from '../Header';
+import './Transaction.css';
+
+export default class Transaction extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			loading: true,
+			transactions: {
+				asdfa2b3c3a: {
+					title: 'Cab fare',
+					id: 'asdfa2b3c3a',
+					amount: '500',
+					between: ['1a2b3c3a', '275dssdg', '4dsgdssdg'],
+					paidBy: '1a2b3c3a'
+				},
+				'23ewi': {
+					title: 'Cab fare',
+					id: '23ewi',
+					amount: '200',
+					between: ['1a2b3c3a', '275dssdg', '4dsgdssdg'],
+					paidBy: '1a2b3c3a'
+				}
+			},
+			eventId: '23ewi',
+			membersId: [],
+			members: {}
+		};
+		this.fetchMembers = this.fetchMembers.bind(this);
+	}
+
+	componentWillMount() {
+		fetch('http://localhost:8080/members')
+			.then(response => response.json())
+			.then(data => {
+				console.log(data);
+				this.setState({ members: data, loading: false });
+			})
+			.catch(() => {
+				console.log('Aww Snap !!');
+			});
+	}
+
+	fetchMembers = id => {
+		console.log('members', this.state.members);
+		return this.state.members[id].name;
+	};
+
+	render() {
+		let transactions = this.state.transactions;
+		let transactionId = Object.keys(transactions);
+		//const member=members[0];
+		let allMyTransactions = <div />;
+		if (this.state.loading == false && transactions.length != 0) {
+			console.log('Aaaayaaaa');
+			allMyTransactions = transactionId.map(id => {
+				const transaction = transactions[id];
+				transaction['between'] = transaction['between'].map(this.fetchMembers);
+				console.log('Transaction betweeen', transaction.between);
+				return <TransactionCard>{transaction}</TransactionCard>;
+			});
+		}
+
+		return (
+			<div className="App">
+				<Header>Transactions</Header>
+				<div className="dashboard-container">{allMyTransactions}</div>
+			</div>
+		);
+	}
+}
+
+const TransactionCard = props => {
+	const items = props.children;
+	return Card(
+		<div className="transaction-item">
+			<h3>{items.title}</h3>
+			<Accordian>
+				<ul>{items.between.map(member => <li>{member}</li>)}</ul>
+			</Accordian>
+		</div>
+	);
+};
+
+class Accordian extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			visible: true
+		};
+	}
+
+	render() {
+		let stuff = 'Click to view';
+		if (this.state.visible) {
+			stuff = this.props.children;
+		}
+		return (
+			<div>
+				<button
+					onClick={() => {
+						this.setState({ visible: !this.state.visible });
+					}}
+				>
+					Toggle
+				</button>
+				<div>{stuff}</div>
+			</div>
+		);
+	}
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import Register from "./Register";
-import registerServiceWorker from "./registerServiceWorker";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import Transaction from './Transaction';
+import Dashboard from './Dashboard';
+import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Register />, document.getElementById("root"));
+ReactDOM.render(<Transaction />, document.getElementById('root'));
 registerServiceWorker();
